f025ef5a64263f8b5c10bc517ac58da81802b74d
Nitesh yadav <79129812+Niteshy85013@users.noreply.github.com>
2023-10-14 13:35:59 +0545

    updated

--- client/src/components/Home/Banner2.js
+++ client/src/components/Home/Banner2.js
@@ -5,33 +5,37 @@ const Banner2 = () => {
     <section className="banner2">
       <div className="container ">
         <div className="row">
-          <div className="col-md-6">
+          <div className="col-md-6 banner2cont">
             <div className="col py-5 ms-5">
-              <h1 className="fw-bold">CSS</h1>
-              <p className="text-dark">The language for styling web pages</p>
-              <div>
-                <button
-                  type="submit"
-                  className="btn btn-success rounded mt-4 fw-bold border-0"
-                >
-                  Learn CSS
-                </button>
-              </div>
-              <div>
-                <button
-                  type="submit"
-                  className="btn btn-dark rounded mt-4 fw-bold border-0"
-                >
-                  CSS References
-                </button>
-              </div>
+              <h1 className="fw-bold txt">CSS</h1>
+              <p className="text-dark txt fs-4 fst-italic">
+                The language for styling web pages
+              </p>
+              <div className="txt">
+                <div>
+                  <button
+                    type="submit"
+                    className="btn btn-success rounded mt-4 fw-bold border-0"
+                  >
+                    Learn CSS
+                  </button>
+                </div>
+                <div>
+                  <button
+                    type="submit"
+                    className="btn btn-dark rounded mt-4 fw-bold border-0"
+                  >
+                    CSS References
+                  </button>
+                </div>
 
-              <button
-                type="submit"
-                className="btn btn-success rounded mt-4 fw-bold b2button mb-3"
-              >
-                Get Certified
-              </button>
+                <button
+                  type="submit"
+                  className="btn btn-success rounded mt-4 fw-bold b2button mb-3"
+                >
+                  Get Certified
+                </button>
+              </div>
             </div>
           </div>
           <div className="col-md-6">
