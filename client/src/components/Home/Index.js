7a8a6cbc21992d6daf64ac2afa01c2413f1e187a
Nitesh yadav <79129812+Niteshy85013@users.noreply.github.com>
2023-10-15 21:42:07 +0545

    updated

--- client/src/components/Home/Index.js
+++ client/src/components/Home/Index.js
@@ -5,30 +5,47 @@ const Home = () => {
   return (
     <div>
       <div className="container-fluid curved-container">
-        <main className="text-center py-5">
-          <p className="heading text-light fw-bold head1">Learn to Code</p>
-          <p className="text hcolor mt-3">
-            The World's Largest Developers' Sites.
-          </p>
-          <form className="d-flex justify-content-center mt-3" role="search">
-            <div className="input-group">
-              <input
-                type="text"
-                className="form-control rounded border border-success border-2"
-                placeholder="Search tutorials, e.g. Java"
-                aria-label="Search"
-              />
-              <button className="btn btn-success" type="submit">
-                <i className="bi bi-search"></i>
-              </button>
-            </div>
-          </form>
-          <div>
-            <p className="hsec text-light fw-bold mt-5 ">
-              Not Sure Where to Begin?
-            </p>
+        <div className="row">
+          <div className="col-md-6">
+            <main className="text-center py-5 mt-5">
+              <p className="heading text-light fw-bold head1 pos">
+                Learn To Code
+              </p>
+              <p className="text hcolor mt-3">
+                The World's Largest Developers' Sites.
+              </p>
+              <form
+                className="d-flex justify-content-center mt-3"
+                role="search"
+              >
+                <div className="input-group">
+                  <input
+                    type="text"
+                    className="form-control rounded border border-success border-2"
+                    placeholder="Search tutorials, e.g. Java"
+                    aria-label="Search"
+                  />
+                  <button className="btn btn-success" type="submit">
+                    <i className="bi bi-search"></i>
+                  </button>
+                </div>
+              </form>
+              <div>
+                <p className="hsec text-light fw-bold mt-5 ">
+                  Not Sure Where to Begin?
+                </p>
+              </div>
+            </main>
           </div>
-        </main>
+          <div className="col-md-6 ">
+            <img
+              src="./images/home4.png"
+              alt=""
+              className="img-fluid"
+              style={{ height: "30rem" }}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
