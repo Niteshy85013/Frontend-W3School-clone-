f1ee74011987215929e0ab9beb7d917a778c0759
Nitesh yadav <79129812+Niteshy85013@users.noreply.github.com>
2023-10-15 11:49:51 +0545

    random update

--- client/src/components/Home/Index.js
+++ client/src/components/Home/Index.js
@@ -1,27 +1,30 @@
 import React from "react";
 import "./home.css";
+
 const Home = () => {
   return (
     <div>
-      <div className="container-fluid curved-container">
-        <main class="text-center py-5">
-          <p class="heading text-light fw-bold head1">Learn to Code</p>
-          <p class="text hcolor mt-3">The World largest Developers Sites.</p>
+      <div className="container curved-container">
+        <main className="text-center py-5">
+          <p className="heading text-light fw-bold head1">Learn to Code</p>
+          <p className="text hcolor mt-3">
+            The World's Largest Developers' Sites.
+          </p>
           <form className="d-flex justify-content-center mt-3" role="search">
-            <div class="input-group ">
+            <div className="input-group">
               <input
                 type="text"
-                class="form-control rounded border-success border-2 "
+                className="form-control rounded border border-success border-2"
                 placeholder="Search tutorials, e.g. Java"
                 aria-label="Search"
               />
-              <button class="btn btn-success " type="button">
-                <i class="bi bi-search"></i>
+              <button className="btn btn-success" type="submit">
+                <i className="bi bi-search"></i>
               </button>
             </div>
           </form>
           <div>
-            <p className="hsec text-light fw-bold mt-5">
+            <p className="hsec text-light fw-bold mt-5 ">
               Not Sure Where to Begin?
             </p>
           </div>
